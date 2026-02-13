aac3a8300c0528b6685e2b08093c2c0ceab50007
filthypotato <tylorromine94@gmail.com>
2026-02-13 06:16:32 -0800

    CLI.py started

---
+++ cli.py
@@ -0,0 +1,22 @@
+
+
+
+def main():
+    print("==== Welcome to AttackLab ====\n")
+
+    print("Select an attack scenario to simulate:\n")
+    print("1) SSH Brute Force")
+    print("2) Cron Persistence")
+    print("3) Data Exfiltration Staging")
+    print("4) Exit\n")
+
+    choice = int(input("Enter choice (1-4): "))
+
+    if choice < 1 or choice > 4:
+        print("Not a valid input range.")
+
+    print(f"\nYou selected option: {choice}")
+
+
+if __name__ == "__main__":
+    main()
